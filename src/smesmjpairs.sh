028bfe38c25f6c234ea7d80a86e26bce6e4347f1
Trond Trosterud <trond.trosterud@uit.no>
2007-10-26 13:01:08 +0000

    made a dual shellscript-makefile setup to make a smesmj transducer.

--- src/smesmjpairs.sh
+++ src/smesmjpairs.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 # Script to pair sme and smj after smj analysis
-# To be written with variable filenames later
+# This file is called from the makefile.
 
-# Usage: Be in words/dicts/smesmj/src/. 
-# Write:
-# ./smesmjpairs.sh
-# The resulting file can then be picked up after a minute or three.
+echo "LEXICON Root" > ../bin/smesmj.lexc
 
-for f1 in `cat inc-smj-ok-oddverbs.txt | cut -f1`
-do
-	grep $f1 inc-smesmj-alloddverbs.txt >> inc-smesmj-ok-oddverbs.txt
-done
+grep -v "\#" smesmj.xml | grep [a-z] | tr -d '7' | cut -f1 > ../tmp/smesmj-sme
 
-# The unknowns:
-#for f1 in `cat inc-smj-unknown-oddverbs.txt`
-#do
-#	grep $f1 inc-smesmj-alloddverbs.txt >> inc-smesmj-unknown-oddverbs.txt
-#done
+grep -v "\#" smesmj.xml | grep [a-z] | cut -f2 > ../tmp/smesmj-smj
+
+paste -d":" ../tmp/smesmj-sme ../tmp/smesmj-smj | sed 's/$/ \# ;/g' >> ../bin/smesmj.lexc
+
+
+
+# Old version, obsolete:
+# # Usage: Be in words/dicts/smesmj/src/. 
+# # Write:
+# # ./smesmjpairs.sh
+# # The resulting file can then be picked up after a minute or three.
+# 
+# for f1 in `cat inc-smj-ok-oddverbs.txt | cut -f1`
+# do
+# 	grep $f1 inc-smesmj-alloddverbs.txt >> inc-smesmj-ok-oddverbs.txt
+# done
+# 
+# # The unknowns:
+# #for f1 in `cat inc-smj-unknown-oddverbs.txt`
+# #do
+# #	grep $f1 inc-smesmj-alloddverbs.txt >> inc-smesmj-unknown-oddverbs.txt
+# #done
