798e96333e2a31116ef98bc8902ae8788776eedc
Trond Trosterud <trond.trosterud@uit.no>
2007-11-15 08:47:47 +0000

    txt

--- src/smesmjpairs.sh
+++ src/smesmjpairs.sh
@@ -5,9 +5,9 @@
 
 echo "LEXICON Root" > ../bin/smesmj.lexc
 
-grep -v "\#" smesmj.xml | grep [a-z] | tr -d '7' | cut -f1 > ../tmp/smesmj-sme
+grep -v "\#" smesmj.txt | grep [a-z] | tr -d '7' | cut -f1 > ../tmp/smesmj-sme
 
-grep -v "\#" smesmj.xml | grep [a-z] | cut -f2 > ../tmp/smesmj-smj
+grep -v "\#" smesmj.txt | grep [a-z] | cut -f2 > ../tmp/smesmj-smj
 
 paste -d":" ../tmp/smesmj-sme ../tmp/smesmj-smj | sed 's/$/ \# ;/g' >> ../bin/smesmj.lexc
 
