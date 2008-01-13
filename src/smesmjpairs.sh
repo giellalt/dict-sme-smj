5795cb7d2266c0db178f287b4cc13def7bc0d6b5
Trond Trosterud <trond.trosterud@uit.no>
2008-01-13 10:29:16 +0000

    removed old stuff

--- src/smesmjpairs.sh
+++ src/smesmjpairs.sh
@@ -13,19 +13,3 @@ paste -d":" ../tmp/smesmj-sme ../tmp/smesmj-smj | sed 's/$/ \# ;/g' >> ../bin/sm
 
 
 
-# Old version, obsolete:
-# # Usage: Be in words/dicts/smesmj/src/. 
-# # Write:
-# # ./smesmjpairs.sh
-# # The resulting file can then be picked up after a minute or three.
-# 
-# for f1 in `cat inc-smj-ok-oddverbs.txt | cut -f1`
-# do
-# 	grep $f1 inc-smesmj-alloddverbs.txt >> inc-smesmj-ok-oddverbs.txt
-# done
-# 
-# # The unknowns:
-# #for f1 in `cat inc-smj-unknown-oddverbs.txt`
-# #do
-# #	grep $f1 inc-smesmj-alloddverbs.txt >> inc-smesmj-unknown-oddverbs.txt
-# #done
