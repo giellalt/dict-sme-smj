5710bb7fcf129d6d60a256dc755e8fd1d32b9d84
Kevin Unhammer <unhammer@fsfe.org>
2015-02-06 12:05:50 +0000

    utf-8 output+strictwarnings; follow dtd like smenob

--- scripts/smesmj2xml.pl
+++ scripts/smesmj2xml.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+# -*- mode: cperl; indent-tabs-mode: nil; cperl-indent-level: 4; -*-
 #
 # kvasikode
 #
@@ -21,38 +22,54 @@
 
 ########################################################
 
-use encoding 'utf-8';
+use strict;
+use warnings;
+use Encode;
+use encoding::warnings;
+use utf8;
+use v5.14;
+use autodie;
+use open qw( :encoding(UTF-8) :std );
+
+binmode( STDIN, ':utf8' );
+binmode( STDOUT, ':utf8' );
+binmode( STDERR, ':utf8' );
+
+$ENV{'PERL_UNICODE'}="S";
+
 
 # File and directory variables:
-$SRCDIR = "../src";
+my $dict_file = "../bin/smesmj.xml";
 
-$DICT = $SRCDIR . "/../bin/smesmj.xml";
+open (my $dict_h, ">", $dict_file) or die "Can't open $dict_file: $!\n";
+binmode($dict_h, ":encoding(UTF-8)");
 
-open DICT, ">$DICT" or die "Can't open file $DICT: $!\n";
+print $dict_h "<r id=\"smesmj\" xml:lang=\"sme\">\n";
 
-print DICT "<dictionary >\n";
-
-while ( $line = <> ) {
+while ( my $line = <> ) {
 	chomp $line;
-	($sme,
-	 $smj,
-     $POS)
-	   = split /\t/, $line ;
-#    print "Lina er: $line\n";
-#    print "Lemma er: $Lemma\n";
-#    $Lemma =~ s/"\W?(\w)/$1/; # rens $Lemma for " og ^L
-	@smj = split /,/, $smj;
-    print DICT "\t<entry>\n";
-    print DICT "\t  <lemma POS=\"$POS\">$sme</lemma>\n";
-    print DICT "\t  <mgr>\n";
-    print DICT "\t    <trgr>\n";
-    foreach ( @smj ) {
-    print DICT "\t      <trans decl=\"$POS\">$_</trans>\n";
-    }
-    print DICT "\t    </trgr>\n";
-    print DICT "\t  </mgr>\n";
-    print DICT "\t</entry>\n";
+	my ($sme,
+        $smj,
+        $POS)
+      = split /\t/, $line ;
+    #    print "Lina er: $line\n";
+    #    print "Lemma er: $Lemma\n";
+    #    $Lemma =~ s/"\W?(\w)/$1/; # rens $Lemma for " og ^L
+	my @smj = split /,/, $smj;
+    my $tg = join "\n", map {"            <t pos=\"$POS\">$_</t>"} @smj;
+    print $dict_h <<EOS
+   <e>
+      <lg>
+         <l pos=\"$POS\">$sme</l>
+      </lg>
+      <mg>
+         <tg>
+$tg
+         </tg>
+      </mg>
+   </e>
+EOS
 }
 
-print DICT "</dictionary>\n";
-close DICT;
+print $dict_h "</r>\n";
+close $dict_h;
